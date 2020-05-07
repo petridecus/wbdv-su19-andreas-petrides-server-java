2f4ddc7a97d50b8f3565be251d2cade1d00c7488
petridecus <apetrides32@gmail.com>
2020-05-07 04:27:47 -0400

    fixed spring functionality

--- src/main/java/com/example/hw1/DemoApplication.java
+++ src/main/java/com/example/hw1/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.hw1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
