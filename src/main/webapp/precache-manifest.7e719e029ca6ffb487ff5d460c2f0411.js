578311c9d0a1c106c96073ce3be1a46fb41079ed
petridecus <apetrides32@gmail.com>
2020-05-26 17:12:41 -0400

    first commit with react content

---
+++ src/main/webapp/precache-manifest.7e719e029ca6ffb487ff5d460c2f0411.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb975e06cb2bcc455bd1f98b4b7d8169",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc4175577c3f16967da2",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "1c91f612b5c255b7a965",
+    "url": "/static/css/main.6ee95320.chunk.css"
+  },
+  {
+    "revision": "bc4175577c3f16967da2",
+    "url": "/static/js/2.9e2f184c.chunk.js"
+  },
+  {
+    "revision": "618d9e067b5f7c0ee048a43ae1109b7d",
+    "url": "/static/js/2.9e2f184c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c91f612b5c255b7a965",
+    "url": "/static/js/main.662f0536.chunk.js"
+  },
+  {
+    "revision": "8351da8f0e2eb909a01c",
+    "url": "/static/js/runtime-main.07461152.js"
+  }
+]);
