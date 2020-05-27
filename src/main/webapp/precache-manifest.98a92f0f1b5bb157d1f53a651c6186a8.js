9d15c0ee31dedd98ec9f0df0f750e6fce37e8443
petridecus <apetrides32@gmail.com>
2020-05-27 02:14:57 -0400

    final commit for hw3/server commit w second prod build

---
+++ src/main/webapp/precache-manifest.98a92f0f1b5bb157d1f53a651c6186a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82973d43b863798e138ae8eed24b039d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "643cc378247595e1d607",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "07e8be89e73d8f444c21",
+    "url": "/static/css/main.4383bc49.chunk.css"
+  },
+  {
+    "revision": "643cc378247595e1d607",
+    "url": "/static/js/2.be2f3a08.chunk.js"
+  },
+  {
+    "revision": "618d9e067b5f7c0ee048a43ae1109b7d",
+    "url": "/static/js/2.be2f3a08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07e8be89e73d8f444c21",
+    "url": "/static/js/main.9b749a6f.chunk.js"
+  },
+  {
+    "revision": "8351da8f0e2eb909a01c",
+    "url": "/static/js/runtime-main.07461152.js"
+  }
+]);
--- src/main/webapp/service-worker.js
+++ src/main/webapp/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e719e029ca6ffb487ff5d460c2f0411.js"
+  "/precache-manifest.98a92f0f1b5bb157d1f53a651c6186a8.js"
 );
 
 self.addEventListener('message', (event) => {
