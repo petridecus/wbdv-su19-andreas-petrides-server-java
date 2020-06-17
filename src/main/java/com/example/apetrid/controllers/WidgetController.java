1fefa9f5cee91edf1ae13fd85f4addbeb2f09149
petridecus <apetrides32@gmail.com>
2020-06-17 17:26:17 -0400

    cross origin fix

--- src/main/java/com/example/apetrid/controllers/WidgetController.java
+++ src/main/java/com/example/apetrid/controllers/WidgetController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class WidgetController {
     @Autowired
     WidgetService widgetService;
