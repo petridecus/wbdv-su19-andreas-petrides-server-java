36b25d29adaaea220278f21e60dfb0ada88bb6ff
petridecus <apetrides32@gmail.com>
2020-06-17 17:41:09 -0400

    tried again

--- src/main/java/com/example/apetrid/controllers/WidgetController.java
+++ src/main/java/com/example/apetrid/controllers/WidgetController.java
@@ -8,6 +8,7 @@ import com.example.apetrid.models.WidgetModel;
 import com.example.apetrid.services.WidgetService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
